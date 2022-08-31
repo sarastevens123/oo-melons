b85bca8ee7bb28dba183aec07c2b2d2fd6cc6b09
Sara Stevens <sarastevens@Saras-MacBook-Air.local>
2022-08-30 20:59:34 -0700

    added TooManyMelonsError class

--- melons.py
+++ melons.py
@@ -1,3 +1,7 @@
+
+from random import randint
+from datetime import datetime
+
 """Classes for melon orders."""
 class AbstractMelonOrder:
     order_type = None
@@ -9,11 +13,27 @@ class AbstractMelonOrder:
         self.shipped = False
         self.christmas_time = False
 
+
+    def get_base_price(self):
+        base_price = randint(5,9)
+        return base_price
+
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        if self.qty > 100:
+            raise TooManyMelonsError("No more than 100 melons!")
+
+        base_price = self.get_base_price()
         
+        current_time = datetime.now()
+        # if current_time.weekday() in range(5)
+        # if (0 <= current_time.weekday() <= 4)
+           # if (8 <= current_time.hour <= 11):
+        if (0 <= current_time.weekday() <= 4) and (8 <= current_time.hour <= 11):
+            base_price += 4 
+
         if self.christmas_time:
             base_price = 1.5 * base_price
         
@@ -33,12 +53,11 @@ class AbstractMelonOrder:
 
         self.shipped = True
 
-    def is_christmas_time(self):
-        """determines if christmas time"""
+    #Change the method time and update the docstring
+    def set_christmas_time(self):
+        """Sets christmas time to True if called"""
 
         self.christmas_time = True
-
-
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -51,7 +70,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     order_type = "international"
     tax = 0.17
-    
+
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
@@ -63,7 +82,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
         return self.country_code
 
-    
+
 class GovernmentMelonOrder(AbstractMelonOrder):
     order_type = "government"
     
@@ -71,7 +90,14 @@ class GovernmentMelonOrder(AbstractMelonOrder):
         super().__init__(species, qty)
         self.passed_inspection = False
 
+
     def mark_inspection(self, passed):
         self.passed_inspection = passed 
 
-    
+
+class TooManyMelonsError(ValueError):
+    pass
+    # Inherits from ValueError and returns error when melons > 100"""""
+
+# try 
+# except TooManyMelonsError
